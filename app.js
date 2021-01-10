c81f6f69fc67c9e33cd92eac46534713be8d6c22
Viktor <65297330+LOOKATmixer@users.noreply.github.com>
2021-01-10 12:27:35 +0500

    project12.iteration2

--- app.js
+++ app.js
@@ -4,14 +4,12 @@ const bodyParser = require("body-parser");
 const { PORT = 3000 } = process.env;
 
 const app = express();
-const usersRoute = require("./routes/users");
-const cardsRoute = require("./routes/cards");
+const routes = require("./routes/index");
 
 app.use("/", express.static("public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/users", usersRoute);
-app.use("/cards", cardsRoute);
+app.use(routes);
 app.use((req, res) => {
   res.status(404).send({ message: "Запрашиваемый ресурс не найден" });
 });
